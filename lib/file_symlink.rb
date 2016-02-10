732a318c52bfa8888e73e6ec7d4d8ae51d619cc4
Glenn Sisson <glenn.sisson@gmail.com>
2016-02-10 08:22:38 -0800

    codeclimate recommended fixes

--- lib/file_symlink.rb
+++ lib/file_symlink.rb
@@ -7,7 +7,7 @@ class << File
       #windows mklink syntax is reverse of unix ln -s
       #windows mklink is built into cmd.exe
       #vulnerable to command injection, but okay because this is a hack to make a cli tool work.
-      stdin, stdout, stderr, wait_thr = Open3.popen3('cmd.exe', "/c mklink /d #{new_name.gsub('/','\\')} #{old_name.gsub('/','\\')}")
+      _stdin, _stdout, _stderr, wait_thr = Open3.popen3('cmd.exe', "/c mklink /d #{new_name.tr('/','\\')} #{old_name.tr('/','\\')}")
       wait_thr.value.exitstatus
     else
       self.old_symlink(old_name, new_name)
