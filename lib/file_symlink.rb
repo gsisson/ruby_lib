5e8612031d87e80e6d4c74b4904b1b3d5cee41ff
Glenn Sisson <glenn.sisson@gmail.com>
2016-02-09 19:48:51 -0800

    symlink for PC; use lib/ dir

---
+++ lib/file_symlink.rb
@@ -0,0 +1,16 @@
+class << File
+  alias_method :old_symlink,  :symlink
+
+  def symlink(old_name, new_name)
+    #if on windows, call mklink, else self.symlink
+    if RUBY_PLATFORM =~ /mswin32|cygwin|mingw|bccwin/
+      #windows mklink syntax is reverse of unix ln -s
+      #windows mklink is built into cmd.exe
+      #vulnerable to command injection, but okay because this is a hack to make a cli tool work.
+      stdin, stdout, stderr, wait_thr = Open3.popen3('cmd.exe', "/c mklink /d #{new_name.gsub('/','\\')} #{old_name.gsub('/','\\')}")
+      wait_thr.value.exitstatus
+    else
+      self.old_symlink(old_name, new_name)
+    end
+  end
+end
