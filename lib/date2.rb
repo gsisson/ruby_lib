4715a2268865deff9e0e4830281e2a53f4969ffb
Glenn Sisson <glenn.sisson@gmail.com>
2016-02-12 19:43:59 -0800

    reduce complexity

--- lib/date2.rb
+++ lib/date2.rb
@@ -49,37 +49,42 @@ class Date2
     end
     nil
   end
+  def self.handle_date_time(match)
+    year, _x, month, _x, day, _x, hours, _x, minutes, _x, seconds, ampm = match.captures
+    # 0    1    2     3   4    5    6     7     8      9    10      11
+    # ^^^^^^^^^^^^^^  the captures  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    if ampm && ampm =~ /PM/
+      hours = (hours.to_i + 12).to_s
+    end
+    if day.length > 2 # 2008-01-31 <= 01-31-2008
+      year, month, day = day, year, month
+    end
+    month   = "0#{month}"   if   month.length == 1
+    day     = "0#{day}"     if     day.length == 1
+    hours   = "0#{hours}"   if   hours.length == 1
+    minutes = "0#{minutes}" if minutes.length == 1
+    seconds = "0#{seconds}" if seconds.length == 1
+    return "#{year}-#{month}-#{day}_#{hours}.#{minutes}.#{seconds}"
+  end
+  def self.handle_date(match)
+    year,  _x, month, _x, day = match.captures
+    # 0     1    2     3   4
+    # ^^    the captures  ^^
+    if day.length > 2 # 2008-01-31 <= 01-31-2008
+      year, month, day = day, year, month
+    end
+    month   = "0#{month}"   if   month.length == 1
+    day     = "0#{day}"     if     day.length == 1
+    return "#{year}-#{month}-#{day}"
+  end
   def self.corrected_prefix_for(date_or_date_time, pattern_date_time, pattern_date)
     match = date_or_date_time.match(pattern_date_time)
     if match
-      year, _x, month, _x, day, _x, hours, _x, minutes, _x, seconds, ampm = match.captures
-      # 0    1    2     3   4    5    6     7     8      9    10      11
-      # ^^^^^^^^^^^^^^  the captures  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-      if ampm && ampm =~ /PM/
-        hours = (hours.to_i + 12).to_s
-      end
-      if day.length > 2 # 2008-01-31 <= 01-31-2008
-        year, month, day = day, year, month
-      end
-      month   = "0#{month}"   if   month.length == 1
-      day     = "0#{day}"     if     day.length == 1
-      hours   = "0#{hours}"   if   hours.length == 1
-      minutes = "0#{minutes}" if minutes.length == 1
-      seconds = "0#{seconds}" if seconds.length == 1
-      return "#{year}-#{month}-#{day}_#{hours}.#{minutes}.#{seconds}"
+      return handle_date_time(match)
     end
     match = date_or_date_time.match(pattern_date)
-    #puts "match.nil?: #{match.nil?}"
     if match
-      year,  _x, month, _x, day = match.captures
-      # 0     1    2     3   4
-      # ^^    the captures  ^^
-      if day.length > 2 # 2008-01-31 <= 01-31-2008
-        year, month, day = day, year, month
-      end
-      month   = "0#{month}"   if   month.length == 1
-      day     = "0#{day}"     if     day.length == 1
-      return "#{year}-#{month}-#{day}"
+      return handle_date(match)
     end
     nil
   end
