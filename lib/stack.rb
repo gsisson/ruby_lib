742f3328b319ba9d1f13663e92be9e385a764c7e
Glenn Sisson <glenn.sisson@gmail.com>
2016-04-23 18:42:46 -0700

    adds Stack#backtrace

--- lib/stack.rb
+++ lib/stack.rb
@@ -15,4 +15,40 @@ module Stack
       f.sub(%r{#{ENV['HOME']}},'~').sub(/^/,prefix)
     end
   end
+  def self.backtrace e
+    # Returns a backtrace, without the excessively long lines that are hard to read.
+    # Usage: Since a backtrace is only of use for unhandled exceptions, Call this from your
+    #   last rescue clause (which usually just catches the base exception class "Exception"
+    # Example code:
+    #   Here is example code calling this method in its 'unhandled exception' handler:
+    #
+    #     begin
+    #       some_code
+    #     rescue SomeExceptionType => e
+    #       handle_someExceptionType
+    #     rescue SomeOtherExceptionType => e
+    #       handle_someOtherExceptionType
+    #     rescue Exception => e
+    #       $stderr.puts ScriptHelper.backtrace e
+    #       abort
+    #     end
+    #
+    msg = []
+    msg << redbold('ERROR:')+' Unexpected Exception:'
+    msg << "       (#{e.class}) "+redbold("#{e.message}")
+    msg << "       backtrace: "
+    rubypath=""
+    e.backtrace.each do |i|
+      next if i =~ /.*\/ruby\/gems\//
+      next if i =~ /.*\/\.rbenv\//
+      msg << "         #{i.sub(ENV['HOME'], '~')}"
+    end
+    msg
+  end
+  private
+  def self.redbold str
+    bold_red="\e[1m\e[31m"
+    exit_color="\e[0m"
+    "#{bold_red}#{str}#{exit_color}"
+  end
 end
--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -1,22 +1,23 @@
 require 'spec_helper'
 require_relative '../lib/stack'
 
-def level3
+def level3(throw_exception)
+  throw Exception if throw_exception
   Stack.frames()
 end
 
-def level2
-  level3
+def level2(throw_exception)
+  level3(throw_exception)
 end
 
-def level1
-  level2
+def level1(throw_exception)
+  level2(throw_exception)
 end
 
-describe "Friendly" do
+describe "Stack" do
   describe '#frames' do
     it 'should return stack frames' do
-      frames = level1
+      frames = level1(false)
       # ~/some/path/stack_trace_spec.rb:5:in `level3'
       # ~/some/path/stack_trace_spec.rb:9:in `level2'
       # ~/some/path/stack_trace_spec.rb:13:in `level1'
@@ -24,6 +25,25 @@ describe "Friendly" do
       expect(frames[0]).to match(/#{File.basename(__FILE__)}:[[:digit:]]+:in .level3/i)
       expect(frames[1]).to match(/#{File.basename(__FILE__)}:[[:digit:]]+:in .level2/i)
       expect(frames[2]).to match(/#{File.basename(__FILE__)}:[[:digit:]]+:in .level1/i)
+    end
+  end
+  describe '#backtrace' do
+    it 'should have stack info after exception throw' do
+      trace = nil
+      begin
+        throw_exception = true
+        level1(throw_exception)
+      rescue Exception => e
+        trace = Stack.backtrace(e)
+      end
+      #trace.each { |l| $stderr.puts ":#{l}:" }
+      expect(trace.size).to_not be(0)
+      expect(trace[0]).to match('Unexpected Exception')
+      expect(trace[1]).to match('UncaughtThrowError')
+      expect(trace[2]).to match('backtrace:')
+      trace[3..-1].each do |trace|
+        expect(trace).to match(File.basename(__FILE__))
+      end
     end
   end
 end
