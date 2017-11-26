b48edda38bd0a061faf628b5c7beaf48a4554b60
Glenn Sisson <glenn.sisson@gmail.com>
2017-11-25 19:15:59 -0800

    new minor scripts

---
+++ rename.file.if.seen.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+
+file_target="file1.dat"
+file_source="#{file_target}.tmp"
+
+puts "watching for file (#{file_source})..."
+loop do
+  if File.file? file_source
+    puts "  found!"
+    if File.file? file_target
+      puts "  Warning! Cannot rename, as the target file exists! (#{file_target})"
+    else
+      print "  renaming (#{file_source} to #{file_target})..."
+      begin
+        FileUtils.mv file_source, file_target
+        puts " success!"
+      rescue Exception => ex
+        puts " ERROR! (#{ex.message})"
+      end
+    end
+    puts "resuming watch for file (#{file_source})..."
+  end
+  sleep 1 # second
+end
