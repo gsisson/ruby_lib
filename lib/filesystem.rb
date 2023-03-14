1baca3836e98fe7e85a49dbf79da3ac81a15995d
Glenn Sisson <glennaws@amazon.com>
2023-03-13 17:13:19 -0700

    updates

--- lib/filesystem.rb
+++ lib/filesystem.rb
@@ -3,7 +3,46 @@
 # are unique. Will stop with error if two incoming paths
 # have the same basename()
 
+require 'open3'
+require 'fileutils'
+require '~/usr/ruby/lib/dir2.rb'
+
 class FS
+  VIDEO_EXTENSIONS = /f4v$|webm$|m2v$|m2t$|flv$|mts$|m4v$|avi$|3gp$|wmv$|mov$|mp4$|mpg$|mpeg$|mkv$/i
+  def self.mkshortcut_in_cwd(tgt)
+    tgt = tgt.sub("t:/","/cygdrive/t/")
+    # puts "in dir: #{Dir.pwd}"
+    # puts "  running Open3.popen3('mkshortcut.exe','#{tgt}')"
+    _stdin, _stdout, _stderr, wait_thr = Open3.popen3('mkshortcut.exe', "#{tgt}")
+    # wait_thr.value.exitstatus # (this will cause a long pause)
+  end
+  #the -w option to mkshortcut.exe does not work!
+  #def self.mkshortcut_over_there(tgt, working_dir)
+  #  tgt = tgt.sub("t:/","/cygdrive/t/")
+  #  puts "mkshortcut.exe -w #{working_dir} #{tgt}"
+  #  _stdin, _stdout, _stderr, wait_thr = Open3.popen3('mkshortcut.exe', "-w #{working_dir}", "#{tgt}")
+  #  # wait_thr.value.exitstatus # (this will cause a long pause)
+  #end
+  def self.all_vids_recursively(dir)
+    vids = []
+    Dir.chdir dir do
+      vids = Dir2.all_files_recursively()
+      vids.select! {|i| ! Dir.exist?(i)}
+      vids.map! {|i|"#{dir}#{i}"}
+      # select only file types interested
+      vids.select! {|i| i =~ FS::VIDEO_EXTENSIONS }
+      FS.verify_no_dup_files_across_dirs(vids)
+    end
+    vids
+  end
+  def self.all_vids_recursively_as_hash(dir)
+    vids = self.all_vids_recursively(dir)
+    vids_hash = {}
+    vids.each do |vid|
+      vids_hash[File.basename(vid)] = vid
+    end
+    vids_hash
+  end
   def self.verify_no_dup_files_across_dirs(vids)
     hash = {}
     probs = []
@@ -16,12 +55,27 @@ class FS
       end
     end
     if probs.size > 0
-      STDERR.puts("ERROR: found dup-named vids:")
+      STDERR.puts("ERROR (in filesystem.rb):")
+      STDERR.puts("  found dup-named vids:")
       probs.each do |prob|
         puts "       #{prob}"
         puts "       #{hash[File.basename(prob)]}"
       end
       exit 1
+    end
+  end
+  def self.shortcuts_remove_if_exist_in_subdir
+    sub_lnks = Dir2.glob_i('**/*.lnk').select { |item| item =~ /\//}
+    sub_lnks_hash={}
+    sub_lnks.each do |lnk|
+      sub_lnks_hash[File.basename(lnk)] = true
+    end
+    lcl_lnks = Dir2.glob_i('*.lnk')
+    lcl_lnks.each do |lnk|
+      if sub_lnks_hash[lnk]
+        # puts "File.rm #{lnk}"
+        FileUtils.rm lnk
+      end
     end
   end
 end
---
+++ shortcuts_remove_if_exist_in_subdir.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+require '~/usr/ruby/lib/filesystem'
+
+FS.shortcuts_remove_if_exist_in_subdir()
+
